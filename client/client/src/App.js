7db772f4ef88c5b989ed6b0f720f7eeb859aa374
victor <victor.murithi@student.moringaschool.com>
2024-03-07 19:42:33 +0300

    styling the intro

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Layout/Navbar';
+import Intro from './Components/Intro';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
---
+++ client/client/src/Components/Intro.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../Css/Intro.css';
+import bg from '../Images/bg-remove.png';
+import { Link } from 'react-scroll';
+
+export default function Intro() {
+  return (
+    <section id='intro'>
+      <div className='intro-content'>
+        <span className='hello'>Hello,</span>
+        <span className='IntroText'>
+          I'm <span className='introName'>Victor Murithi</span>
+          <br />
+          Software Engineer
+        </span>
+        <p className='introPara'>
+          I'm a software engineer with a passion for learning and creating web applications that solve real-world problems.
+        </p>
+        <Link>
+          <button className='introBtn'>Hire me</button>
+        </Link>
+      </div>
+      <div className="image-container">
+        <img src={bg} alt='profile' className='bg-image' />
+      </div>
+    </section>
+  );
+}
