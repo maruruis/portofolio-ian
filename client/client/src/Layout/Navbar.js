bb595335ae772ffbeb2930981b496d67bada5554
victor <victor.murithi@student.moringaschool.com>
2024-03-07 16:48:44 +0300

    added bootstrap

--- client/client/src/Layout/Navbar.js
+++ client/client/src/Layout/Navbar.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../Css/Navbar.css"
 
-export default function Navbar() {
-    return (
-        <div className="navbar">
-            <h1>Navbar</h1>
-        </div>
-    );
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
