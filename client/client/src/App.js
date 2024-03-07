d70d55b686ea37e05415c7fbf4095cd4043f4e5f
victor <victor.murithi@student.moringaschool.com>
2024-03-07 15:44:13 +0300

    added some pages

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import Portfolio from './Pages/Portfolio';
+import Resume from './Pages/Resume';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </Router>
   );
