df87381fbf075ece69211d79b3bc26e226a399f2
victor <victor.murithi@student.moringaschool.com>
2024-03-08 12:44:13 +0300

    updated all the styling

--- client/client/src/Components/Intro.js
+++ client/client/src/Components/Intro.js
@@ -7,16 +7,20 @@ export default function Intro() {
   return (
     <section id='intro'>
       <div className='intro-content'>
-        <span className='hello'>Hello,</span>
-        <span className='IntroText'>I'm <span className='introName'>Victor</span><br />
-          a Software Engineer
-        </span>
-        <p className='introPara'>
-          I'm a software engineer with a passion for learning and<br/> creating web applications that solve real-world problems.
-        </p>
-        <Link>
-          <button className='introBtn'>Hire me</button>
-        </Link>
+        <div className='intro-text'>
+          <span className='hello'>Hello,</span>
+          <span className='introText'>
+            I'm <span className='introName'>Victor</span>,<br />
+            a Software Engineer
+          </span>
+          <p className='introPara'>
+            Welcome to my portfolio! I'm a dedicated software engineer with a passion for crafting innovative solutions to real-world problems.
+            With a keen eye for detail and a drive for excellence, I thrive on turning ideas into reality through elegant and efficient web applications.
+          </p>
+          <Link>
+            <button className='introBtn'>Hire me</button>
+          </Link>
+        </div>
       </div>
       <img src={bg} alt='profile' className='bg-image' />
     </section>
