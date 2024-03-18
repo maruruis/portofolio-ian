2bd44da541d67e8285d48c925864dfad19ff01d2
Ian Kimani <ianmwng@gmail.com>
2024-03-18 11:42:29 +0300

    made changes

--- client/client/src/Components/Experience.js
+++ client/client/src/Components/Experience.js
@@ -1,41 +1,12 @@
-import React,{useRef} from 'react';
-import logo from "../Images/Cellulant-logo.jpg";
+import React from 'react';
+import logo from "../Images/iebc.jpeg";
 import facebook from "../Images/Facebook-icon.jpeg";
 import twitter from "../Images/twitter-icon.jpeg";
 import github from "../Images/Github.jpeg";
 import instagram from "../Images/instagram.jpeg";
 import "../Css/Experience.css";
-import emailjs from '@emailjs/browser';
-import Swal from 'sweetalert2';
-
 
 export default function Experience() {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_d5d9gjs', 'template_7rbb78k', form.current, {
-        publicKey: 'NNfkLo0zoAZ75x1Jo',
-      })
-      .then(
-        () => {
-          Swal.fire({
-            icon: 'success',
-            title: 'Email sent successfully',
-          })
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Something went wrong!',
-          })
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
   return (
     <section id='experience'>
       <div id='experienceContent'>
@@ -45,26 +16,24 @@ export default function Experience() {
       <div className='experienceCards'>
         <div className='experienceCard'>
           <img src={logo} alt='Company' className='experienceCardImage' />
-          <p className='experienceCardText'>Cellulant, Kenya</p>
-          <p className='experienceCardText'>Intern Software Engineer</p>
+          <p className='experienceCardText'>IEBC, Kenya</p>
+          <p className='experienceCardText'>ICT Officer, Ruaraka constituency</p>
           <p className='experienceCardText'>May 2022 - August 2022</p>
         </div>
       </div>
       <div id='contact'>
         <h1 className='contactTitle'>Contact Me</h1>
-        <span className='contactDesc'> If you have any questions or would like to work together, please fill out the form below.</span>
-        <form ref={form} className='contactForm' onSubmit={sendEmail}>
-          <input type='text' placeholder=' Your Name' name='your_name' className='name' />
-          <input type='email' placeholder='Your Email' className='email' name='your_email' />
-          <textarea name='message' placeholder='Your Message' className='msg' rows ='5' />
-          <button className='contactBtn' type='submit' value='Send'>Send</button>
-          <div className='links'>
-            <img src={facebook} alt='facebook' className='logo'/>
-            <img src={instagram} alt='instagram' className='logo'/>
-            <img src={twitter} alt='twitter' className='logo'/>
-            <img src={github} alt='github' className='logo'/>
-          </div>
-        </form>
+        <span className='contactDesc'>If you have any questions or would like to work together, please feel free to reach out:</span>
+        <div className='contactDetails'>
+          <p className='contactInfo'>Phone: 0722691871</p>
+          <p className='contactInfo'>Email: ianmwng@gmail.com</p>
+        </div>
+        <div className='links'>
+          <img src={facebook} alt='facebook' className='logo'/>
+          <img src={instagram} alt='instagram' className='logo'/>
+          <img src={twitter} alt='twitter' className='logo'/>
+          <img src={github} alt='github' className='logo'/>
+        </div>
       </div>
     </section>
   );
