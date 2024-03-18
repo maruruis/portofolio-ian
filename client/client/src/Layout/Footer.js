2be64aebd6f3d4321e96f10e629da6af6a71c4b5
Ian Kimani <ianmwng@gmail.com>
2024-03-18 14:40:20 +0300

    portolio

--- client/client/src/Layout/Footer.js
+++ client/client/src/Layout/Footer.js
@@ -3,7 +3,7 @@ import '../Css/Footer.css';
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>© 2024 Victor Murithi. All rights reserved.</p>
+            <p>© 2024 Ian Kimani. All rights reserved.</p>
         </footer>
     );
 }
